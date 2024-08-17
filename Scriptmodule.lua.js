0be47fcf89f57c685e32eeba100147d56004f688
Thesliceofpizza <isaiah.gause2@gmail.com>
2024-08-17 19:53:59 -0400

    Add files via upload

---
+++ Scriptmodule.lua.js
@@ -0,0 +1,13 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: deep-brown; icon-glyph: magic;
+lua
+-- ScriptModule.lua
+local ScriptModule = {}
+
+function ScriptModule.protect()
+    print("Protections applied.")
+    -- Your protection logic here
+end
+
+return ScriptModule
