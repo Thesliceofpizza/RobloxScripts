4f362573d5ca86d0d04c64a3934dbfb5967deb7f
Thesliceofpizza <isaiah.gause2@gmail.com>
2024-08-17 19:53:05 -0400

    Create Scriptmodule.lua

---
+++ Scriptmodule.lua
@@ -0,0 +1,10 @@
+lua
+-- ScriptModule.lua
+local ScriptModule = {}
+
+function ScriptModule.protect()
+    print("Protections applied.")
+    -- Your protection logic here
+end
+
+return ScriptModule
